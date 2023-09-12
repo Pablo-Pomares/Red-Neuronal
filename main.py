7853548d17a223c0efb85029860281bb83783293
Pablo-Pomares <pablo.pomaresv@alumno.buap.mx>
2023-09-11 21:37:26 -0600

    Agruegué la captura de pantalla y probé Adam contra SGD

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import network
 training_data, test_data, _ = load_data_wrapper()
 
 net = network.Network([784, 30, 10])
-net.adam(training_data, 10, 10, 0.001, test_data=test_data)
+net.SGD(training_data, 10, 10, 0.001, test_data=test_data)
 
 # with open("net.h", "w") as file:
 #    file.write(f"{net.weights},{net.biases}")
